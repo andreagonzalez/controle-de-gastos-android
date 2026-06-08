d5e6ec51a0644ea9349c35895a37a449768cee7e
Andrea Veronica González <andreavgonzalez7@gmail.com>
2026-06-08 17:33:39 -0300

    feat: adiciona tela de extrato financeiro

--- app/src/main/java/br/inf/andreagonzalez/controledegastos/ui/ExtratoActivity.java
+++ app/src/main/java/br/inf/andreagonzalez/controledegastos/ui/ExtratoActivity.java
@@ -16,16 +16,36 @@ import br.inf.andreagonzalez.controledegastos.R;
 import br.inf.andreagonzalez.controledegastos.adapter.MovimentoAdapter;
 import br.inf.andreagonzalez.controledegastos.model.Movimento;
 
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+
+import br.inf.andreagonzalez.controledegastos.model.Entrada;
+import br.inf.andreagonzalez.controledegastos.model.Gasto;
+
+
 public class ExtratoActivity extends AppCompatActivity {
     private RecyclerView recyclerMovimentos;
     private MovimentoAdapter adapter;
     private ArrayList<Movimento> listaMovimentos = new ArrayList<>();
+    private SharedPreferences preferences;
+    private ArrayList<Gasto> listaGastos = new ArrayList<>();
+    private ArrayList<Entrada> listaEntradas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_extrato);
+
+        inicializarPreferencias();
+        recuperarListaGastos();
+        recuperarListaEntradas();
+        consolidarMovimentos();
+
         recyclerMovimentos = findViewById(R.id.recyclerMovimentos);
 
         recyclerMovimentos.setLayoutManager(
@@ -33,6 +53,7 @@ public class ExtratoActivity extends AppCompatActivity {
         );
 
         adapter = new MovimentoAdapter(listaMovimentos);
+        adapter.notifyDataSetChanged();
 
         recyclerMovimentos.setAdapter(adapter);
 
@@ -41,5 +62,79 @@ public class ExtratoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void inicializarPreferencias() {
+
+        preferences = getSharedPreferences(
+                "dados",
+                MODE_PRIVATE
+        );
+    }
+
+    private void recuperarListaGastos() {
+
+        Gson gson = new Gson();
+
+        String json =
+                preferences.getString("lista_gastos", null);
+
+        if (json != null) {
+
+            Type type =
+                    new TypeToken<ArrayList<Gasto>>() {}.getType();
+
+            listaGastos.clear();
+
+            listaGastos.addAll(
+                    gson.fromJson(json, type)
+            );
+        }
+    }
+
+    private void recuperarListaEntradas() {
+
+        Gson gson = new Gson();
+
+        String json =
+                preferences.getString("lista_entradas", null);
+
+        if (json != null) {
+
+            Type type =
+                    new TypeToken<ArrayList<Entrada>>() {}.getType();
+
+            listaEntradas.clear();
+
+            listaEntradas.addAll(
+                    gson.fromJson(json, type)
+            );
+        }
+    }
+    private void consolidarMovimentos() {
+
+        listaMovimentos.clear();
+
+        for (Entrada entrada : listaEntradas) {
+
+            listaMovimentos.add(
+                    new Movimento(
+                            entrada.getDescricao(),
+                            entrada.getValor(),
+                            "ENTRADA"
+                    )
+            );
+        }
+
+        for (Gasto gasto : listaGastos) {
+
+            listaMovimentos.add(
+                    new Movimento(
+                            gasto.getDescricao(),
+                            gasto.getValor(),
+                            "GASTO"
+                    )
+            );
+        }
     }
 }
--- app/src/main/java/br/inf/andreagonzalez/controledegastos/ui/MainActivity.java
+++ app/src/main/java/br/inf/andreagonzalez/controledegastos/ui/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -28,9 +28,12 @@ import br.inf.andreagonzalez.controledegastos.adapter.GastoAdapter;
 import br.inf.andreagonzalez.controledegastos.model.Entrada;
 import br.inf.andreagonzalez.controledegastos.model.Gasto;
 import br.inf.andreagonzalez.controledegastos.model.Movimento;
+import br.inf.andreagonzalez.controledegastos.ui.ExtratoActivity;
 
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -47,8 +50,10 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Gasto> listaGastos = new ArrayList<>();
     private ArrayList<Entrada> listaEntradas = new ArrayList<>();
     private ArrayList<Movimento> listaMovimentos = new ArrayList<>();
+
     private RecyclerView recyclerView;
     private GastoAdapter adapter;
+    private Button btnVerExtrato;
     private void adicionarEntrada() {
 
         String descricao = editDescricaoEntrada.getText().toString();
@@ -195,6 +200,7 @@ public class MainActivity extends AppCompatActivity {
         editDescricaoEntrada = findViewById(R.id.editDescricaoEntrada);
         editValorEntrada = findViewById(R.id.editValorEntrada);
         btnAdicionarEntrada = findViewById(R.id.btnAdicionarEntrada);
+        btnVerExtrato = findViewById(R.id.btnVerExtrato);
     }
     private void configurarSpinnerCategorias() {
 
@@ -420,11 +426,10 @@ public class MainActivity extends AppCompatActivity {
                 listaGastos.remove(position);
                 adapter.notifyItemRemoved(position);
 
-                float salarioSalvo = preferences.getFloat("salario", 0);
-                double saldo = salarioSalvo - totalGasto;
+                recalcularSaldo();
 
                 textTotalGasto.setText("Total gasto: " + formatarMoeda(totalGasto));
-                atualizarSaldo(saldo);
+
 
                 salvarListaGastos();
 
@@ -454,6 +459,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Botão adicionar gasto
         btnAdicionarGasto.setOnClickListener(v -> adicionarGasto());
+
+        // Botão ver extrato
+        btnVerExtrato.setOnClickListener(v -> {
+
+            Intent intent = new Intent(
+                    MainActivity.this,
+                    ExtratoActivity.class
+            );
+
+            startActivity(intent);
+        });
     }
 
     // =========================
@@ -514,11 +530,7 @@ public class MainActivity extends AppCompatActivity {
                         formaPagamentoSelecionada
                 );
 
-        Toast.makeText(
-                this,
-                "Pagamento: " + formaPagamentoSelecionada,
-                Toast.LENGTH_SHORT
-        ).show();
+
 
         listaGastos.add(novoGasto);
         adapter.notifyItemInserted(listaGastos.size() - 1);
@@ -526,7 +538,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(
                 this,
-                "Categoria: " + categoriaSelecionada,
+                "Gasto adicionado com sucesso",
                 Toast.LENGTH_SHORT
         ).show();
 
