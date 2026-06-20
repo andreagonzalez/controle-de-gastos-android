cfee5bcdc12a52653e1fdb7bf06a0ad28898d812
Andrea Veronica González <andreavgonzalez7@gmail.com>
2026-06-20 10:48:40 -0300

    feat: adiciona tela de listagem de entradas

---
+++ app/src/main/java/br/inf/andreagonzalez/controledegastos/adapter/EntradaAdapter.java
@@ -0,0 +1,104 @@
+package br.inf.andreagonzalez.controledegastos.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import br.inf.andreagonzalez.controledegastos.R;
+import br.inf.andreagonzalez.controledegastos.model.Entrada;
+
+public class EntradaAdapter
+        extends RecyclerView.Adapter<EntradaAdapter.EntradaViewHolder> {
+
+    private ArrayList<Entrada> listaEntradas;
+
+    public EntradaAdapter(ArrayList<Entrada> listaEntradas) {
+        this.listaEntradas = listaEntradas;
+    }
+
+    @NonNull
+    @Override
+    public EntradaViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent,
+            int viewType
+    ) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_entrada, parent, false);
+
+        return new EntradaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull EntradaViewHolder holder,
+            int position
+    ) {
+
+        Entrada entrada = listaEntradas.get(position);
+
+        holder.textDescricao.setText(
+                entrada.getDescricao()
+        );
+
+        holder.textValor.setText(
+                formatarMoeda(entrada.getValor())
+        );
+
+        holder.textData.setText(
+                "Data: " + entrada.getData()
+        );
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaEntradas.size();
+    }
+
+    private String formatarMoeda(double valor) {
+
+        NumberFormat formatoBrasil =
+                NumberFormat.getCurrencyInstance(
+                        new Locale("pt", "BR")
+                );
+
+        return formatoBrasil.format(valor);
+    }
+
+    public static class EntradaViewHolder
+            extends RecyclerView.ViewHolder {
+
+        TextView textDescricao;
+        TextView textValor;
+        TextView textData;
+
+        public EntradaViewHolder(
+                @NonNull View itemView
+        ) {
+            super(itemView);
+
+            textDescricao =
+                    itemView.findViewById(
+                            R.id.textDescricaoEntrada
+                    );
+
+            textValor =
+                    itemView.findViewById(
+                            R.id.textValorEntrada
+                    );
+
+            textData =
+                    itemView.findViewById(
+                            R.id.textDataEntrada
+                    );
+        }
+    }
+}
